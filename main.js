5838977fd4cfaf3aba2bc12d619ad718b3c6a319
adeekahh <ekydesignlab@gmail.com>
2019-03-09 15:22:10 +0100

    front page styles and work image js

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ let aboutText = document
     return /\S/.test(str);
   });
 
+//Work image arrays
+let intermediaArray = ["1.png", "2.png", "3.png"];
+
 window.addEventListener("DOMContentLoaded", init());
 
 //Starting functions
@@ -38,4 +41,35 @@ function showAboutText() {
 //About section restart button
 function showRestartButton() {
   document.querySelector(".about-restart-button").style.opacity = "1";
+}
+
+//Work image script
+document.querySelector("#intermedia").addEventListener("mouseover", () => {
+  console.log(event);
+  loopThroughImages();
+});
+document.querySelector("#intermedia").addEventListener("mouseleave", () => {
+  console.log(event);
+  hideWorkImage();
+});
+
+function loopThroughImages() {
+  document.querySelector(".work-image").style.display = "block";
+  for (let i = 0; i < intermediaArray.length; i++) {
+    (function(i) {
+      setTimeout(function() {
+        document.querySelector(
+          ".work-image"
+        ).src = `assets/project-pictures/intermedia/${intermediaArray[i]}`;
+
+        if (i >= intermediaArray.length) {
+          loopThroughImages();
+        }
+      }, 300 * i);
+    })(i);
+  }
+}
+
+function hideWorkImage() {
+  document.querySelector(".work-image").style.display = "none";
 }
